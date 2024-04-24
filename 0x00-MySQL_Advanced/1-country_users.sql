79b4508bed7d92dd0a11074e4542f2bf34f9d105
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-24 17:40:03 +0100

    1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,17 +1,8 @@
--- Create a table users with the specified attributes if it doesn't already exist
+-- Create a table 'users' with country enumeration if it doesn't exist
 
--- Check if the table exists
-IF NOT EXISTS (
-    SELECT *
-    FROM information_schema.tables
-    WHERE table_schema = DATABASE()
-    AND table_name = 'users'
-) THEN
-    -- Create the table
-    CREATE TABLE users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255),
-        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-    );
-END IF;
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);

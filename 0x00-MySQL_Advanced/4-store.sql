8c74aa6f1971298de41511343eb5e641b412c274
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-24 17:52:36 +0100

    4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,27 +1,17 @@
--- Drop the trigger if it already exists
-DROP TRIGGER IF EXISTS decrease_quantity_trigger;
-
 -- Create a trigger to decrease item quantity after adding a new order
-DELIMITER //
-CREATE TRIGGER decrease_quantity_trigger AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END//
-DELIMITER ;
 
--- Create tables if they don't exist
+-- Drop the trigger if it already exists
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS orders;
+
 CREATE TABLE IF NOT EXISTS items (
     name VARCHAR(255) NOT NULL,
-    quantity INT NOT NULL DEFAULT 10
+    quantity int NOT NULL DEFAULT 10
 );
 
 CREATE TABLE IF NOT EXISTS orders (
     item_name VARCHAR(255) NOT NULL,
-    number INT NOT NULL
+    number int NOT NULL
 );
 
--- Insert initial data into the items table
 INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");

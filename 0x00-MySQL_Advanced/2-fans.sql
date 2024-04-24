3fa44edb6958c4d10d48589a02340103dd11bf74
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-24 17:46:50 +0100

    2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- Rank country origins of bands by the number of fans
 
 -- Calculate the number of fans for each country origin
-SELECT origin, COUNT(*) as nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;

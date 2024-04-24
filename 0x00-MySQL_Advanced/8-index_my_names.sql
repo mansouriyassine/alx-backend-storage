ec95da774ed4ded20ea8704f55285d9b66a460dc
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-24 17:56:22 +0100

    4

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,4 @@
 -- Create an index idx_name_first on the first letter of the name column in the names table
 
--- Drop the index if it already exists
-DROP INDEX IF EXISTS idx_name_first ON names;
-
--- Create the index
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first
+ON names(name(1));

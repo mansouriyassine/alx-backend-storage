eca1b9b808f7422439db2e9e54a350c64caa9eb4
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-24 18:03:09 +0100

    1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Create a table 'users' with country enumeration if it doesn't exist
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM ('US', 'CO', 'TN') NOT NULL
 );

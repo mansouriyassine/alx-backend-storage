ffefbba306b07ef0eac2f57175fe92bd6b6401d3
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-24 18:01:28 +0100

    10

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,10 +1,6 @@
 -- Create a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 
--- Drop the view if it already exists
 DROP VIEW IF EXISTS need_meeting;
-
--- Create the view
 CREATE VIEW need_meeting AS
-SELECT name
-FROM students
-WHERE (score < 80 OR last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+SELECT name FROM students WHERE score < 80
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));

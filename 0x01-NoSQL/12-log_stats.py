dba4a28125261ef5848c08cff558b9fc1e5240ce
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-29 18:52:34 +0100

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Script to provide stats about Nginx logs stored in MongoDB
+"""
+
+from pymongo import MongoClient
+
+def log_stats():
+    """
+    Provides some stats about Nginx logs stored in MongoDB
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+if __name__ == "__main__":
+    log_stats()

770bbf43fc4fe82ee6524f4a5d7aa04102269bc7
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-29 18:55:35 +0100

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python3
 """
-Script to provide stats about Nginx logs stored in MongoDB
+Nginx Log Statistics
 """
 
 from pymongo import MongoClient
+from typing import Tuple
 
-def log_stats():
+
+def get_nginx_statistics() -> Tuple:
     """
-    Provides some stats about Nginx logs stored in MongoDB
+    Queries the nginx collection for specific data
+    - Returns:
+        - Total count of log documents
+        - Counts of each HTTP method in the collection
+        - Count of GET calls to /status path
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client: MongoClient = MongoClient()
     db = client.logs
     collection = db.nginx
+    http_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    method_counts = []
+    for method in http_methods:
+        count = collection.count_documents({'method': method})
+        method_counts.append({'method': method, 'count': count})
+    total_count = collection.estimated_document_count()
+    status_check_count = collection.count_documents({'method': 'GET', 'path': '/status'})
+    client.close()
+    return total_count, method_counts, status_check_count
 
-    total_logs = collection.count_documents({})
 
-    print(f"{total_logs} logs")
+def print_nginx_statistics() -> None:
+    """
+    Prints statistics from nginx log queries
+    """
+    total_count, method_counts, status_check_count = get_nginx_statistics()
+    print(f'Total logs: {total_count}')
+    print('HTTP Methods:')
+    for method in method_counts:
+        print(f'\t{method.get("method")}: {method.get("count")}')
+    print(f'GET /status requests: {status_check_count}')
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
 
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
-
-if __name__ == "__main__":
-    log_stats()
+if __name__ == '__main__':
+    print_nginx_statistics()

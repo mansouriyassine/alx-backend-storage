bba1e2838d9ec8ec0bbd67ae21d5f3df5bff383d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-29 19:27:16 +0100

    102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,67 +1,65 @@
 #!/usr/bin/env python3
 """
-Log stats - new version
+Script to provide stats about Nginx logs stored in MongoDB
 """
+
 from pymongo import MongoClient
 
 
-def get_nginx_stats(mongo_collection):
+def nginx_log_stats():
     """
-    Queries nginx collection for specific data.
-
-    Args:
-        mongo_collection: pymongo collection object representing the nginx
-        collection.
-
-    Returns:
-        tuple: A tuple containing:
-            - The count of all documents in the collection.
-            - A dictionary containing the count of each method in the collection.
-            - A dictionary containing the top 10 most present IPs in the collection.
+    Function to retrieve stats about Nginx logs
     """
-    doc_count = mongo_collection.count_documents({})
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-    method_stats = {}
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        method_count = mongo_collection.count_documents({'method': method})
-        method_stats[method] = method_count
+    total_logs = collection.count_documents({})
 
-    top_ips = {}
+    http_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    method_counts = {}
+    for method in http_methods:
+        count = collection.count_documents({'method': method})
+        method_counts[method] = count
+
+    status_check_count = collection.count_documents({
+        'method': 'GET',
+        'path': '/status'
+        })
+
+    # Top 10 IPs
     pipeline = [
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ]
-    ip_results = mongo_collection.aggregate(pipeline)
-    for ip_result in ip_results:
-        top_ips[ip_result["_id"]] = ip_result["count"]
+    top_ips = collection.aggregate(pipeline)
 
-    return doc_count, method_stats, top_ips
+    client.close()
+
+    return total_logs, method_counts, status_check_count, top_ips
 
 
-def print_nginx_stats(doc_count, method_stats, top_ips):
+def print_nginx_stats():
     """
-    Prints stats from nginx query.
-
-    Args:
-        doc_count (int): Total count of documents.
-        method_stats (dict): Dictionary containing the count of each method.
-        top_ips (dict): Dictionary containing the top 10 most present IPs.
+    Function to print stats about Nginx logs
     """
-    print(f'{doc_count} logs')
-    print('Methods:')
-    for method, count in method_stats.items():
-        print(f'\tmethod {method}: {count}')
-    print('IPs:')
-    for ip, count in top_ips.items():
-        print(f'\t{ip}: {count}')
+    total_logs, method_counts, status_check_count, top_ips = nginx_log_stats()
+
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
+
+    print(f"{status_check_count} status check")
+
+    print("IPs:")
+    for ip_data in top_ips:
+        ip = ip_data["_id"]
+        count = ip_data["count"]
+        print(f"\t{ip}: {count}")
 
 
-if __name__ == '__main__':
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-
-    doc_count, method_stats, top_ips = get_nginx_stats(logs_collection)
-
-    print_nginx_stats(doc_count, method_stats, top_ips)
+if __name__ == "__main__":
+    print_nginx_stats()

6d509a378aa6fd13527c25cb7c3b976fdcf38758
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-29 18:39:29 +0100

    8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module containing function to list all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+
+    Args:
+        mongo_collection: PyMongo collection object
+
+    Returns:
+        list: List of all documents in the collection
+    """
+    documents = mongo_collection.find({})
+    
+    return list(documents)

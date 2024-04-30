bb3b8c94fbeb2a085561caf48a62dbfe0dd8fc13
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-30 18:18:08 +0100

    5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,46 +4,35 @@ Web cache and tracker
 """
 import requests
 import redis
-import time
 from functools import wraps
 
-# Initialize Redis connection
-redis_conn = redis.Redis()
+# Establish Redis connection
+cache_store = redis.Redis()
 
-def track_access(func):
-    """
-    Decorator to track the number of times a URL is accessed
-    """
+def track_url_access(func):
+    """Decorator to track URL access"""
     @wraps(func)
     def wrapper(url):
-        key = f"count:{url}"
-        redis_conn.incr(key)
-        return func(url)
+        """Wrapper function to track URL access"""
+        cached_key = "cached:" + url
+        cached_data = cache_store.get(cached_key)
+        
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html_content = func(url)
+
+        cache_store.incr(count_key)
+        cache_store.set(cached_key, html_content)
+        cache_store.expire(cached_key, 10)
+
+        return html_content
+
     return wrapper
 
-def cache_page(expiration=10):
-    """
-    Decorator to cache the result of a function with expiration time
-    """
-    def decorator(func):
-        @wraps(func)
-        def wrapper(url):
-            key = f"cache:{url}"
-            cached_content = redis_conn.get(key)
-            if cached_content:
-                return cached_content.decode('utf-8')
-            else:
-                content = func(url)
-                redis_conn.setex(key, expiration, content)
-                return content
-        return wrapper
-    return decorator
-
-@track_access
-@cache_page()
-def get_page(url: str) -> str:
-    """
-    Retrieve HTML content of a URL and cache it with an expiration time
-    """
+@track_url_access
+def fetch_web_page(url: str) -> str:
+    """Retrieve HTML content of a URL"""
     response = requests.get(url)
     return response.text
